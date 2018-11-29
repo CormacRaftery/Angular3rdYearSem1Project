08b6e68368544a7d78483b1d8f807709c93705a4
CormacRaftery <cormacinho@hotmail.com>
2018-11-29 13:07:53 +0000

    first commit

---
+++ src/app/post-details/post-details.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../services/post.service';
+import { Observable } from 'rxjs';
+import { Post } from '../post.model';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgForm } from "@angular/forms";
+@Component({
+  selector: 'app-post-details',
+  templateUrl: './post-details.component.html',
+  styleUrls: ['./post-details.component.css']
+})
+export class PostDetailsComponent implements OnInit {
+
+
+  posts: any = [];
+  datasaver: any = [];
+  title = "";
+  content = "";
+  constructor(private ps: PostService) { }
+  ngOnInit() {
+
+    this.ps.getPostsData().subscribe(data => {
+      this.datasaver = data;
+      this.posts=[];
+
+    });
+  }
+
+  onSearchPost(form: NgForm) {
+    var k = 0;
+    this.posts = this.datasaver;
+    var indexes: any = [];
+    for (let i in this.datasaver) {
+      if (this.datasaver[i].title != form.value.search) {
+        indexes[k] = i;
+        k++;
+      }
+    }
+    if (k > 0) {
+      while (k > 0) {
+        k--;
+        var removed = this.posts.splice(indexes[k], 1);
+      }
+    }
+    form.resetForm();
+  }
+
+  onDelete(id: String) {
+    console.log("Delete called " + id);
+    this.ps.deletePost(id).subscribe(() => {
+      this.ngOnInit();
+    })
+  }
+}
